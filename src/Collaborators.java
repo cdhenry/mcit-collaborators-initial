f2c44c7ddd246fcb22499e75000ea84da202d4fa
cdhenry <cdhenry@gmail.com>
2019-06-17 11:42:06 -0500

    committed some code

---
+++ src/Collaborators.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Collaborators {
+	public ArrayList<String> names = new ArrayList<String>();
+	
+	public Collaborators(String... args) {
+		for (String name: args) {
+			this.names.add(name);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Collaborators collaborators = new Collaborators("Chris", "Wendy"); 
+		for(String name: collaborators.names) {
+			System.out.println(name);
+		}
+	}
+}
